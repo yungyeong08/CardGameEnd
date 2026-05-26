ff4c1f0c9db63afedadac52a9bde80b9fe052d48
yungyeong08 <wkddbsrud18@gmail.com>
2026-05-26 16:13:42 +0900

    유니티짱

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += Vector3.forward * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += Vector3.back * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position += Vector3.right * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.position += Vector3.left * speed * Time.deltaTime;
+        //}
+    }
+}
